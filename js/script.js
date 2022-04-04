a4871018fa6c2b26dc239b06d10ab848cababe85
Yusuf Ali Selek <yusufaliselek41@gmail.com>
2022-04-05 02:06:39 +0300

    first-commit

---
+++ js/script.js
@@ -0,0 +1,74 @@
+/* Responsive Nav Menu */
+
+const navMenu = () => {
+    const navbarToggler = document.querySelector(".navbar-toggler");
+    const navList = document.querySelector(".nav-list");
+
+    navbarToggler.addEventListener("click", () => {
+        navList.classList.toggle("nav-active");
+        navbarToggler.classList.toggle("toggle");
+    })
+};
+
+navMenu();
+
+/* SERVICES SLIDER */
+
+const serviceChange = () => {
+    let firstChild, lastChild;
+    const arrowLeft = document.querySelector("#arrow-left");
+    const arrowRight = document.querySelector("#arrow-right");
+    const servicesAll = document.querySelector("#services-slider");
+
+    document.addEventListener("click", () => {
+        if (event.target === arrowLeft) {
+            lastChild = servicesAll.lastElementChild;
+            servicesAll.insertAdjacentElement("afterbegin", lastChild);
+        } 
+        else if (event.target === arrowRight){
+            firstChild = servicesAll.firstElementChild;
+            servicesAll.insertAdjacentElement("beforeend", firstChild);
+        }
+    });
+};
+
+serviceChange();
+
+
+/*  SCROLL REVEAL ANIMATION*/
+
+ScrollReveal().reveal(".ani-left", {
+    origin: "left",
+    distance: "10rem",
+    duration: 1000,
+    easing: "ease-in"
+});
+
+ScrollReveal().reveal(".ani-right", {
+    origin: "right",
+    distance: "10rem",
+    duration: 1000,
+    easing: "ease-in"
+});
+
+ScrollReveal().reveal(".ani-fade-up", {
+    opacity: 0,
+    easing: "ease-in"
+});
+
+ScrollReveal().reveal(".ani-bottom", {
+    origin: "bottom", /*aşağıdan gelir*/
+    distance: "10rem",
+    duration: 1000,
+    easing: "ease-in"
+});
+
+ScrollReveal().reveal(".ani-delay", {
+    delay: 900
+});
+
+ScrollReveal().reveal(".ani-delay-2", {
+    delay: 1400
+});
+
+
